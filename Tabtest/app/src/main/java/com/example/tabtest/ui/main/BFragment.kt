d75949a884ffaa76fca8398b50400ca3f971708c
hcp5004 <hcp5004>
2020-12-30 20:30:50 +0900

    Contacts complete

--- Tabtest/app/src/main/java/com/example/tabtest/ui/main/BFragment.kt
+++ Tabtest/app/src/main/java/com/example/tabtest/ui/main/BFragment.kt
@@ -8,12 +8,19 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.tabtest.R
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 
 class BFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_b, container, false)
 //        val textView: TextView = root.findViewById(R.id.section_label)
+        var button: ImageButton = root.findViewById(R.id.add_btn)
+        button.setOnClickListener {
+            Toast.makeText(requireContext(), "테스트", Toast.LENGTH_SHORT ).show()
+        }
         return root
     }
 
