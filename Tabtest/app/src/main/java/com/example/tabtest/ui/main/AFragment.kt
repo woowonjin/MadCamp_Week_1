65ce6d8e907a70e5c069c4b9a62d0937ab2f19dc
hcp5004 <hcp5004>
2020-12-30 18:17:35 +0900

    load contacts #3

--- Tabtest/app/src/main/java/com/example/tabtest/ui/main/AFragment.kt
+++ Tabtest/app/src/main/java/com/example/tabtest/ui/main/AFragment.kt
@@ -17,37 +17,89 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import androidx.lifecycle.Observer
 import android.Manifest
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposables
+import io.reactivex.schedulers.Schedulers
+import androidx.core.content.ContextCompat
+import android.util.Log
+
 
 
 class AFragment : Fragment() {
+    private lateinit var contactsHelper: ContactsHelper
+    private var disposable = Disposables.empty()
+    private val mAdapter = CustomAdapter()
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_a, container, false)
         val textView: TextView = root.findViewById(R.id.section_label)
 
-        var userList = arrayListOf<DataVo>(
-                DataVo("박해철","tesdid","카이스트","10000","user_img_01"),
-                DataVo("박해철","tesdid","카이스트","10000","user_img_01")
-        )
+//        var userList = arrayListOf<DataVo>(
+//                DataVo("박해철","tesdid","카이스트","10000","user_img_01"),
+//                DataVo("박해철","tesdid","카이스트","10000","user_img_01")
+//        )
 
-        val mAdapter=CustomAdapter(requireContext(), userList)
+        contactsHelper = ContactsHelper(requireContext().contentResolver)
+
         val recyler_view: RecyclerView = root.findViewById(R.id.recycler_view)
         recyler_view.adapter = mAdapter
 
         val layout = LinearLayoutManager(requireContext())
-        recyler_view.layoutManager=layout
-        recyler_view.setHasFixedSize(true)
+        recyler_view.layoutManager = layout
+        //recyler_view.setHasFixedSize(true)
 
-
-
-
-
-
-
-
-
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_CONTACTS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.READ_CONTACTS),
+                PERMISSION_READ_CONTACTS
+            )
+        } else {
+            loadContacts()
+        }
         return root
     }
 
+
+    private fun loadContacts() {
+        disposable.dispose()
+        disposable = contactsHelper.getAllContacts().subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                mAdapter.bindItem(it.values.toList())
+            }, { Log.e("ContactHelper", it.message, it) })
+    }
+
+    override fun onDestroy() {
+        disposable.dispose()
+        super.onDestroy()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_READ_CONTACTS -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    loadContacts()
+                }
+            }
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+    companion object {
+        private const val PERMISSION_READ_CONTACTS = 1
+    }
 }
+
+
+
